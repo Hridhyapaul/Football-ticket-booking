319efd33b226c4c4500b2af685cfcba10f662155
Hridhya Paul <Hpdesign2021@gmail.com>
2026-06-19 17:12:21 +0600

    Create Bookings Table and Insert data to the Bookings Table

--- query.sql
+++ query.sql
@@ -107,5 +107,38 @@ VALUES
         'Available'
     );
 
+--------------------------------------------------------------
+-- Create Bookings Table and Insert data to the Bookings Table
+--------------------------------------------------------------
+CREATE TABLE
+    Bookings (
+        booking_id INT PRIMARY KEY,
+        user_id INT NOT NULL,
+        match_id INT NOT NULL,
+        seat_number VARCHAR(20),
+        payment_status VARCHAR(20) CHECK (
+            payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+        ),
+        total_cost DECIMAL(10, 2) NOT NULL CHECK (total_cost >= 0),
+        FOREIGN KEY (user_id) REFERENCES Users (user_id),
+        FOREIGN KEY (match_id) REFERENCES Matches (match_id)
+    );
+
+INSERT INTO
+    Bookings (
+        booking_id,
+        user_id,
+        match_id,
+        seat_number,
+        payment_status,
+        total_cost
+    )
+VALUES
+    (501, 1, 101, 'A-12', 'Confirmed', 150.00),
+    (502, 1, 102, 'B-04', 'Confirmed', 120.00),
+    (503, 2, 101, 'A-13', 'Confirmed', 150.00),
+    (504, 2, 101, NULL, NULL, 150.00),
+    (505, 3, 102, 'C-20', 'Pending', 120.00);
+
 
 
