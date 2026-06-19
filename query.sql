43d03429f67922bb6ec55abd5532a70998f5fbed
Hridhya Paul <Hpdesign2021@gmail.com>
2026-06-19 18:46:06 +0600

    Solved Query 06

--- query.sql
+++ query.sql
@@ -196,3 +196,15 @@ WHERE
 
     select u.user_id, u.full_name, b.booking_id from users as u
     left join bookings as b on u.user_id = b.user_id;
+
+    --------------------------------------------------------------------------------------------------------------------------
+    -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+    --------------------------------------------------------------------------------------------------------------------------
+
+    select booking_id, match_id, total_cost::integer as total_cost from bookings
+    where total_cost > (
+    select
+      avg(total_cost)
+    from
+      Bookings
+    )
